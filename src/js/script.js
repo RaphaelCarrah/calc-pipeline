87e3ff0bc14b02ac180542245bbe980dad133192
Raphael Carrah <Raphael Carrah>
2025-04-05 23:19:22 -0300

    Teste

--- src/js/script.js
+++ src/js/script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
         previousOperand = currentOperand;
         currentOperand = '';
     }
-
+  
     function compute() {
         let computation;
         const prev = parseFloat(previousOperand);
